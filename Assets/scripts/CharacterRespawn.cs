8a54c2a5d118d0da455562d3b7ab1406296a9d1f
s32268 <s32268@pjwstk.edu.pl>
2026-05-11 14:47:37 +0100

    health system

--- Assets/scripts/CharacterRespawn.cs
+++ Assets/scripts/CharacterRespawn.cs
@@ -3,35 +3,44 @@ using UnityEngine;
 public class CharacterRespawn : MonoBehaviour
 {
     [Header("Player Respawn")]
-    public Vector3 respawnPosition;  
+    public Transform respawnPosition;  //Transform
     public float respawnDelay = 0f;  
+    public int maxHealth = 3;
+    private int currentHealth;
 
     [Header("Animation")]
     public Animator animator; 
     public string fallAnimationName = "Fall"; 
-
-    private void Start()
-    {
-        
-        respawnPosition = transform.position;
-    }
+// on health increase decrease
+private void Start()
+{
+    currentHealth = maxHealth;
+}
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
        
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            
-            animator.SetTrigger(fallAnimationName);
-
+            //TODO: dodać logikę odjęcia 1 życia 
            
+            currentHealth -= 1;
+            Debug.Log(currentHealth);
+           
+        }
+
+        if (currentHealth <= 0)
+        {
+            animator.SetTrigger(fallAnimationName);
             Invoke("Respawn", respawnDelay);
+            currentHealth = maxHealth;
+
         }
     }
 
  
     private void Respawn()
     {
-        transform.position = respawnPosition;
+        transform.position = respawnPosition.position;
     }
 }
